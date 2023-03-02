c0a184b945acf860c5c3a3e1b48e7c440b69cebc
Rahul Kumar <87494881+rahuljacker27@users.noreply.github.com>
2023-03-02 16:25:55 +0530

    Question 1
    In this example, the getData function uses the fetch method to make a GET request to the https://jsonplaceholder.typicode.com/posts endpoint, which returns a Promise that resolves to a Response object.
    
    We then use the json method of the Response object to extract the JSON data from the response, which also returns a Promise that resolves to the parsed JSON data.
    
    Finally, we log the JSON data to the console. If an error occurs during the fetch request, we catch the error and log it to the console.
    
    You can customize this example to fetch data from other endpoints of the JSON Placeholder API by replacing the URL used in the fetch method.

---
+++ Question1.js
@@ -0,0 +1,9 @@
+async function getData() {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
